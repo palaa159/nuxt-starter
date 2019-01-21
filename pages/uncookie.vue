f44f3026a8e4ec65ed6f7144d9942645ff19c86c
Apon Palanuwech <palaa159@newschool.edu>
2019-01-21 13:14:04 +0700

    - added univeral-cookie

---
+++ pages/uncookie.vue
@@ -0,0 +1,16 @@
+<template>
+  <div/>
+</template>
+
+<script>
+import Cookies from 'universal-cookie'
+export default {
+  layout: 'unsupported',
+  mounted () {
+    if (process.browser) {
+      new Cookies().remove('__session')
+      return window.location.href = '/'
+    }
+  }
+}
+</script>
